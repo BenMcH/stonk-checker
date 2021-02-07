b0b8651f13fda7706547e893b69b608e866be5d5
Ben McHone <benjamin.mchone@gmail.com>
2021-02-06 19:49:37 -0600

    Respect api limits for yahoo finance

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import * as yahoo from 'https://deno.land/x/yahoo_stock_prices@0.0.1/yahoo-stock-prices.js'
 
-const REFRESH_TIME_IN_SECONDS = 5;
-
 const ticker = [
   'GME',
   'AMC'
@@ -16,4 +14,9 @@ const update = async () => {
 }
 
 update();
-setInterval(update, REFRESH_TIME_IN_SECONDS * 1000)
+
+const MAX_PER_HOUR = 2000;
+const SECONDS_PER_HOUR = 3600;
+
+const interval = SECONDS_PER_HOUR / (MAX_PER_HOUR / ticker.length) * 1000
+setInterval(update, interval)
