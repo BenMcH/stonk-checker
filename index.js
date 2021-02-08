366a4f92afb629886a0c782f8a03b2a9b80106b2
Ben McHone <benjamin.mchone@gmail.com>
2021-02-07 21:55:41 -0600

    Run deno fmt; Add emoji support for easier quick glance trend

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
-import * as yahoo from 'https://deno.land/x/yahoo_stock_prices@0.0.1/yahoo-stock-prices.js'
+import * as yahoo from "https://deno.land/x/yahoo_stock_prices@0.0.1/yahoo-stock-prices.js";
 
 const ticker = [
-  'GME',
-  'AMC'
-]
+  "GME",
+  "AMC",
+];
 
-const log = (message, ...data) => console.log(`[${new Date().toISOString()}]: ${message}`, ...data)
+const log = (message, ...data) =>
+  console.log(`[${new Date().toISOString()}]: ${message}`, ...data);
 
-const update = async () => {
-  for(let symbol of ticker) {
-    await yahoo.getCurrentPrice(symbol).then((price) => log(`${symbol}: ${price}`)).catch((err) => log(`Error fetching: ${symbol}`, err))
-  }
-}
-
-update();
+const wait = (ms) =>
+  new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
 
 const MAX_PER_HOUR = 2000;
 const SECONDS_PER_HOUR = 3600;
 
-const interval = SECONDS_PER_HOUR / (MAX_PER_HOUR / ticker.length) * 1000
-setInterval(update, interval)
+const interval = SECONDS_PER_HOUR / (MAX_PER_HOUR / ticker.length) * 1000 /
+  ticker.length;
+
+const lastPrice = new Map();
+
+while (true) {
+  for (let symbol of ticker) {
+    await yahoo.getCurrentPrice(symbol).then((price) => {
+      const lastValue = lastPrice.get(symbol) || price;
+      lastPrice.set(symbol, price)
+
+      log(
+        `${symbol}: ${price} ${lastValue > price ? '🚀' : lastValue < price ? '💎👐' : '😐'}`,
+      );
+    }).catch((err) => log(`Error fetching: ${symbol}`, err));
+    await wait(interval);
+  }
+}
